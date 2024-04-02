b122f3d0d0367c929bc404c42dafe2da190fa97c
Yevgeny <mr.paranoi@mail.ru>
2024-04-02 23:45:35 +0700

    Last version

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from file import File
 
-# This is The Model
+
 class Model:
     all_notes: list
 
@@ -16,13 +16,23 @@ class Model:
     def list_add_note(self, note):
         number = str(len(self.all_notes) + 1)
         self.all_notes.append(number + ";" + note)
+        print("Заметка добавлена")
        
     def del_note(self, number_note):
         del self.all_notes[number_note]
+        count = 0
+        for note in self.all_notes:
+            count += 1
+            number, title, title_body, time_this = note.split(";")
+            number = str(count)
+            separator = ";"
+            full_note = number + separator + title + separator + title_body + separator + time_this
+            self.all_notes[count-1] = full_note
         print("Заметка удалена")
 
     def file_write(self):
         File().write(self.all_notes)
+        print("Данные сохранены в файл")
 
     def separate_note(self, note):
         separate_note = []
@@ -32,12 +42,18 @@ class Model:
 
     def change_note(self, changed_note, number_note):
         self.all_notes[number_note] = changed_note
+        print("Заметка изменина")
 
-    def list_by_date(self, date):
+    def notes_by_date(self, date):
         filtered_list = []
         for note in self.all_notes:
-            if date in note:
-                filtered_list.append(note)
+            count = 0
+            for part in note.split(";"):
+                count += 1
+                if count == 4:
+                    for date_part in part.split("/"):
+                        if date_part == date:
+                            filtered_list.append(note)
         return filtered_list
 
 
